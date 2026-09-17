b3c3fc82ca8b5414aba0acd2dec2f0cef8742c4e
roylaurie <roy.laurie.pub@sourcetrait.com>
2026-09-17 16:25:44 +0000

    pwsh working on linux

--- shells/powershell/mkpwrhome.ps1
+++ shells/powershell/mkpwrhome.ps1
@@ -2,46 +2,45 @@
 #Requires -Version 7
 param(
     [Parameter(Mandatory)]
-    [System.IO.Path] $dir,
-    [Parameter(Optional)]
+    [string] $dir,
     [string] $owner
 )
 $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
+#Set-PSDebug -Trace 1
 
 function mk_sysusr {
     param(
         [Parameter(Mandatory)]
-        [System.IO.Path] $dir,
-        [Parameter(Optional)]
+        [string] $dir,
         [string] $owner
     )
 
-    New-Item -ItemType Directory -Force -Path $dir
+    $null = New-Item -ItemType Directory -Force -Path $dir
 
     Set-Location -LiteralPath $dir
-    New-Item -ItemType Directory -Force -Path @('.config','bak','data','doc','down','mix','proj','repo','sort','sys','tmp','tpl')
+    $null = New-Item -ItemType Directory -Force -Path @('.config','bak','data','doc','down','mix','proj','repo','sort','sys','tmp','tpl')
 
-    Set-Location -LiteralPath (JoinPath $dir '.config')
-    New-Item -ItemType Directory -Force -Path 'secret'
+    Set-Location -LiteralPath (Join-Path $dir '.config')
+    $null = New-Item -ItemType Directory -Force -Path 'secret'
 
-    Set-Location -LiteralPath (JoinPath $dir 'mix')
-    New-Item -ItemType Directory -Force -Path @('calc','img','mdl','snd','txt','vid','web')
+    Set-Location -LiteralPath (Join-Path $dir 'mix')
+    $null = New-Item -ItemType Directory -Force -Path @('calc','img','mdl','snd','txt','vid','web')
 
-    Set-Location -LiteralPath (JoinPath $dir 'sys')
-    New-Item -ItemType Directory -Force -Path @('cache','data','local','mnt','of','secret','state','srv','sync','use')
+    Set-Location -LiteralPath (Join-Path $dir 'sys')
+    $null = New-Item -ItemType Directory -Force -Path @('cache','data','local','mnt','of','secret','state','srv','sync','use')
 
-    Set-Location -LiteralPath (JoinPath $dir 'sys' 'secret')
-    New-Item -ItemType Directory -Force -Path @('cache','data','state')
+    Set-Location -LiteralPath (Join-Path $dir 'sys' 'secret')
+    $null = New-Item -ItemType Directory -Force -Path @('cache','data','state')
 
-    Set-Location -LiteralPath (JoinPath $dir 'sys' 'local')
-    New-Item -ItemType Directory -Force -Path @('bin','doc','etc','lib','opt','share','src','var')
+    Set-Location -LiteralPath (Join-Path $dir 'sys' 'local')
+    $null = New-Item -ItemType Directory -Force -Path @('bin','doc','etc','lib','opt','share','src','var')
 
-    Set-Location -LiteralPath (JoinPath $dir 'sys' 'sync')
-    New-Item -ItemType Directory -Force -Path @('as','at','me')
+    Set-Location -LiteralPath (Join-Path $dir 'sys' 'sync')
+    $null = New-Item -ItemType Directory -Force -Path @('as','at','me')
 
-    Set-Location -LiteralPath (JoinPath $dir 'sys' 'use')
-    New-Item -ItemType Directory -Force -Path @('asset','cfg','data','doc','exe','lib','pkg','src')
+    Set-Location -LiteralPath (Join-Path $dir 'sys' 'use')
+    $null = New-Item -ItemType Directory -Force -Path @('asset','cfg','data','doc','exe','lib','pkg','src')
     
     if ($owner -ne $null) {
         Set-Location -LiteralPath $dir
@@ -57,58 +56,57 @@ function mk_sysusr {
 function mk_pwrusr {
     param(
         [Parameter(Mandatory)]
-        [System.IO.Path] $dir,
-        [Parameter(Optional)]
+        [string] $dir,
         [string] $owner
     )
 
     mk_sysusr $dir $owner
 
     Set-Location -LiteralPath (Join-Path $dir 'sys' 'data')
-    New-Item -ItemType Directory -Force -Path 'desktop'
+    $null = New-Item -ItemType Directory -Force -Path 'desktop'
 
     Set-Location -LiteralPath (Join-Path $dir 'sys' 'srv')
-    New-Item -ItemType Directory -Force -Path 'git'
+    $null = New-Item -ItemType Directory -Force -Path 'git'
 
     Set-Location -LiteralPath (Join-Path $dir 'sys' 'of')
-    New-Item -ItemType Directory -Force -Path 'nu'
+    $null = New-Item -ItemType Directory -Force -Path 'nu'
 
     Set-Location -LiteralPath (Join-Path $dir 'sys' 'of' 'nu')
-    New-Item -ItemType Directory -Force -Path @('exe','mod')
+    $null = New-Item -ItemType Directory -Force -Path @('exe','mod')
 
     Set-Location -LiteralPath (Join-Path $dir '.config')
-    New-Item -ItemType Directory -Force -Path 'nushell'
+    $null = New-Item -ItemType Directory -Force -Path 'nushell'
 
     Set-Location -LiteralPath (Join-Path $dir '.config' 'nushell')
     if (Test-Path -LiteralPath 'scripts') {
         $retire_dir = mkdtemp (Join-Path $dir 'tmp' 'retire') 'mkpwrhome.'
         $retire_to = Join-Path $retire_dir '.config' 'nushell'
-        New-Item -ItemType Directory -Force -Path $retire_to
+        $null = New-Item -ItemType Directory -Force -Path $retire_to
         Move-Item -LiteralPath 'scripts' -Destination $retire_to
     }
-    New-Item -ItemType SymbolicLink -Path (Join-Path '..' '..' 'sys' 'of' 'nu') -Target 'scripts'
+    $null = New-Item -ItemType SymbolicLink -Path 'scripts' -Target (Join-Path '..' '..' 'sys' 'of' 'nu' 'mod')
 
     Set-Location -LiteralPath (Join-Path $dir 'sys' 'cache')
-    New-Item -ItemType Directory -Force -Path 'cargo'
+    $null = New-Item -ItemType Directory -Force -Path 'cargo'
     
     Set-Location -LiteralPath (Join-Path $dir 'sys' 'cache' 'cargo')
-    New-Item -ItemType Directory -Force -Path 'target'
+    $null = New-Item -ItemType Directory -Force -Path 'target'
 
-    Set-Location -LiteralPath (Join-Path $dir)
-    New-Item -ItemType Directory -Force -Path '.ssh'
+    Set-Location -LiteralPath $dir
+    $null = New-Item -ItemType Directory -Force -Path '.ssh'
 
     Set-Location -LiteralPath (Join-Path $dir '.ssh')
-    New-Item -ItemType Directory -Force -Path 'key'
+    $null = New-Item -ItemType Directory -Force -Path 'key'
 
     Set-Location -LiteralPath (Join-Path $dir '.ssh' 'key')
-    New-Item -ItemType Directory -Force -Path @('as','at','me')
+    $null = New-Item -ItemType Directory -Force -Path @('as','at','me')
 
     Set-Location -LiteralPath (Join-Path $dir '.ssh')
-    if (-not (Test-Path -LiteralPath 'config' -Path Leaf)) {
-        New-Item -ItemType File -Path 'config'
+    if (-not (Test-Path -PathType Leaf -LiteralPath 'config')) {
+        $null = New-Item -ItemType File -Path 'config'
     }
-    if (-not (Test-Path -LiteralPath 'authorized_keys')) {
-        New-Item -ItemType File -Path 'authorized_keys'
+    if (-not (Test-Path -PathType Leaf -LiteralPath 'authorized_keys')) {
+        $null = New-Item -ItemType File -Path 'authorized_keys'
     }
 
     if ($owner -ne $null) {
@@ -126,7 +124,7 @@ function mk_pwrusr {
 function mkdtemp {
     param(
         [Parameter(Mandatory)]
-        [System.IO.Path] $dir,
+        [string] $dir,
         [string] $prefix = 'tmp-'
     )
 
